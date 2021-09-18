0b05242f8ef8f9b766d4663b545609091c96139f
JOHAN STEVEN CARTAGENA GONZALEZ <j.steven.cartagonzalez@gmail.com>
2021-09-18 08:24:56 -0500

    johan

---
+++ comoes.py
@@ -0,0 +1,23 @@
+"""
+el cd permite diferernciar entre numeros positivos y numeros negativos
+09/18/21
+"""
+
+
+numeros = [0,-1.5,12,32,-5.9,-6.3,-7.5,63,47]
+#         0   1  2 3   4    5   6 7 8 9
+print(numeros[4]) 
+    
+if numeros [4] > 0:
+    print("este numero es positivo")
+    
+elif numeros[4] < 0:
+    print("este numero es negativo")
+    
+elif numeros[4] == 0:
+    print("es igual ")
+    
+else:
+    print("es cero ")
+    
+    
